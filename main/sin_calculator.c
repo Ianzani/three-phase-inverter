fca7eb0ef60e2d849cbd081f0f4d58428399aa83
Ianzani <ianzanisirbone@gmail.com>
2024-04-02 17:49:13 -0300

    Modulation logic modified Added new description

--- main/sin_calculator.c
+++ main/sin_calculator.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <math.h>
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -15,6 +14,16 @@ gptimer_handle_t timer_handle = NULL;
 static bool ISR_timer_on_alarm(gptimer_handle_t timer, const gptimer_alarm_event_data_t *edata, void *user_ctx);
 
 
+/**
+ * @brief ISR's 100us periodic timer
+ * 
+ * @param timer : Timer's handle
+ * @param edata : A pointer to a struct with timer's information
+ * @param user_ctx :
+ * 
+ * @retval 0 : A high task has not awoken
+ * @retval 1 : A high task has awoken
+*/
 static bool ISR_timer_on_alarm(gptimer_handle_t timer, 
                                const gptimer_alarm_event_data_t *edata, 
                                void *user_ctx)
@@ -26,24 +35,31 @@ static bool ISR_timer_on_alarm(gptimer_handle_t timer,
     uint16_t phase_1_index;
     uint32_t phase_1_comp;
 
-    uint8_t freq = 50;
+    uint8_t freq = 59;
 
-    master_index += (int32_t)((10000 * 1800 * freq) / (1000000.0));
+    master_index += (int32_t)freq * 18; /* (100 * 100 * freq * 1800) / (10^6) */
 
-    phase_1_index = (uint16_t)round(master_index/100.0);
+    phase_1_index = (uint16_t)(master_index/100.0);
 
     if(phase_1_index >= 1800) {
         phase_1_index -= 1800;
         master_index -= 180000;
     }
 
-    phase_1_comp =(uint16_t)((sine_lookup_table[phase_1_index] * 494) / 65535) + 3;
+    phase_1_comp =(uint16_t)((sine_lookup_table[phase_1_index] * 493.0) / 65535) + 4;
 
     pwm_change_duty(phase_1_comp);
 
     return (high_task_awoken == pdTRUE);
 }
 
+/**
+ * @brief Initialize a preiodic timer with 100us
+ * 
+ * @param None
+ * 
+ * @retval None
+*/
 void sin_init_timer(void)
 {
     gptimer_config_t timer_config = {
