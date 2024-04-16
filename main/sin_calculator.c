a8a00780f9f507041a9e1792d3713224fddc3fcc
Ianzani <ianzanisirbone@gmail.com>
2024-04-16 15:28:32 -0300

    Code organized

--- main/sin_calculator.c
+++ main/sin_calculator.c
@@ -9,49 +9,13 @@
 
 
 gptimer_handle_t timer_handle = NULL;
+volatile uint8_t freq = 60;
 
 
-static bool ISR_timer_on_alarm(gptimer_handle_t timer, const gptimer_alarm_event_data_t *edata, void *user_ctx);
+static bool IRAM_ATTR ISR_timer_on_alarm(gptimer_handle_t timer, 
+                                         const gptimer_alarm_event_data_t *edata, 
+                                         void *user_ctx);
 
-
-/**
- * @brief ISR's 100us periodic timer
- * 
- * @param timer : Timer's handle
- * @param edata : A pointer to a struct with timer's information
- * @param user_ctx :
- * 
- * @retval 0 : A high task has not awoken
- * @retval 1 : A high task has awoken
-*/
-static bool ISR_timer_on_alarm(gptimer_handle_t timer, 
-                               const gptimer_alarm_event_data_t *edata, 
-                               void *user_ctx)
-{
-    BaseType_t high_task_awoken = pdFALSE;
-
-    static volatile int32_t master_index = 0;
-
-    uint16_t phase_1_index;
-    uint32_t phase_1_comp;
-
-    uint8_t freq = 59;
-
-    master_index += (int32_t)freq * 18; /* (100 * 100 * freq * 1800) / (10^6) */
-
-    phase_1_index = (uint16_t)(master_index/100.0);
-
-    if(phase_1_index >= 1800) {
-        phase_1_index -= 1800;
-        master_index -= 180000;
-    }
-
-    phase_1_comp =(uint16_t)((sine_lookup_table[phase_1_index] * 493.0) / 65535) + 4;
-
-    pwm_change_duty(phase_1_comp);
-
-    return (high_task_awoken == pdTRUE);
-}
 
 /**
  * @brief Initialize a preiodic timer with 100us
@@ -84,4 +48,46 @@ void sin_init_timer(void)
 
     gptimer_set_alarm_action(timer_handle, &alarm_config);
     gptimer_start(timer_handle);
+}
+
+void sin_set_freq(uint8_t freq_value)
+{   
+    freq = freq_value;
+}
+
+/**
+ * @brief ISR's 100us periodic timer
+ * 
+ * @param timer : Timer's handle
+ * @param edata : A pointer to a struct with timer's information
+ * @param user_ctx :
+ * 
+ * @retval 0 : A high task has not awoken
+ * @retval 1 : A high task has awoken
+*/
+static bool ISR_timer_on_alarm(gptimer_handle_t timer, 
+                               const gptimer_alarm_event_data_t *edata, 
+                               void *user_ctx)
+{
+    BaseType_t high_task_awoken = pdFALSE;
+
+    static volatile int32_t master_index = 0;
+
+    uint16_t phase_1_index;
+    uint32_t phase_1_comp;
+
+    master_index += (int32_t)freq * 18; /* (100 * 100 * freq * 1800) / (10^6) */
+
+    phase_1_index = (uint16_t)(master_index/100.0);
+
+    if(phase_1_index >= 1800) {
+        phase_1_index -= 1800;
+        master_index -= 180000;
+    }
+
+    phase_1_comp =(uint16_t)((sine_lookup_table[phase_1_index] * 493.0) / 65535) + 4;
+
+    pwm_change_duty(phase_1_comp);
+
+    return (high_task_awoken == pdTRUE);
 }
